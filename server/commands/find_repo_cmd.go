e1507f6f8c4cd3a6098b0b108af34f3ae1284980
xukun <xukun17@sina.com>
2021-11-20 13:14:44 +0800

    完善了 find-repo 命令，让它能接收命令行参数

--- server/commands/find_repo_cmd.go
+++ server/commands/find_repo_cmd.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"strconv"
 
 	"github.com/bitwormhole/starter-cli/cli"
+	"github.com/bitwormhole/starter/collection"
 	"github.com/bitwormhole/starter/io/fs"
 	"github.com/bitwormhole/starter/markup"
 	"github.com/bitwormhole/starter/vlog"
@@ -21,6 +23,7 @@ func (inst *FindRepo) _Impl() (cli.Handler, cli.CommandHelper) {
 	return inst, inst
 }
 
+// GetHelpInfo ...
 func (inst *FindRepo) GetHelpInfo() *cli.CommandHelpInfo {
 	info := &cli.CommandHelpInfo{}
 	info.Name = "find-repo"
@@ -41,15 +44,16 @@ func (inst *FindRepo) Handle(ctx *cli.TaskContext) error {
 	console := ctx.Console
 	task := &findRepoTask{}
 
-	// param
-	task.maxDepth = 5
+	err := inst.parseArgs(ctx, task)
+	if err != nil {
+		return err
+	}
 
 	task.dir = console.GetWorkingPath()
 	task.out = console.Output()
-	task.repoDirNames = []string{".git", ".xgit", ".bitwormhole", ".hole", ".wormhole"}
 	task.results = make([]fs.Path, 0)
 
-	err := task.run()
+	err = task.run()
 	if err != nil {
 		return err
 	}
@@ -58,9 +62,56 @@ func (inst *FindRepo) Handle(ctx *cli.TaskContext) error {
 	return nil
 }
 
+func (inst *FindRepo) parseArgs(ctx *cli.TaskContext, to *findRepoTask) error {
+
+	args := collection.CreateArguments()
+	args.Import(ctx.CurrentTask.Arguments)
+	reader := args.NewReader()
+
+	aTargets := []string{".git", ".xgit", ".bitwormhole", ".hole", ".wormhole"}
+	aMaxDepth := 2
+
+	help := reader.GetFlag("--help")
+	depth := reader.GetFlag("--max-depth")
+	targets := reader.GetFlag("--targets")
+
+	if help.Exists() {
+		to.forHelp = true
+		return nil
+	}
+
+	if depth.Exists() {
+		value, ok := depth.Pick(1)
+		if !ok {
+			return errors.New("bad --max-depth value")
+		}
+		n, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		aMaxDepth = n
+	}
+
+	if targets.Exists() {
+		for i := 1; ; i++ {
+			value, ok := targets.Pick(i)
+			if ok {
+				aTargets = append(aTargets, value)
+			} else {
+				break
+			}
+		}
+	}
+
+	to.repoDirNames = aTargets
+	to.maxDepth = aMaxDepth
+	return nil
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 type findRepoTask struct {
+	forHelp      bool
 	dir          fs.Path
 	maxDepth     int
 	repoDirNames []string
@@ -69,7 +120,16 @@ type findRepoTask struct {
 }
 
 func (inst *findRepoTask) run() error {
+	if inst.forHelp {
+		return inst.printHelpInfo()
+	}
 	return inst.scanDir(inst.dir, 0)
+}
+
+func (inst *findRepoTask) printHelpInfo() error {
+	msg := "usage: find-repo [--help] [--max-depth n] [--targets .git1 .git2 ...]"
+	inst.out.Write([]byte(msg + "\n"))
+	return nil
 }
 
 func (inst *findRepoTask) logResults() {
