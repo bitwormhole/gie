0373787bd1a5c3efeb7e5ff10df82574cfa7dd10
xukun <xukun17@sina.com>
2021-11-20 13:19:02 +0800

    update to v0.0.10

--- module.go
+++ module.go
@@ -16,8 +16,8 @@ import (
 
 const (
 	myModuleName = "github.com/bitwormhole/gie"
-	myModuleVer  = "v0.0.9"
-	myModuleRev  = 13
+	myModuleVer  = "v0.0.10"
+	myModuleRev  = 14
 )
 
 //go:embed src/main/resources
